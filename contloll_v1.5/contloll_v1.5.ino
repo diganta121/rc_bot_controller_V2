c7fb12ec426c9fa04e49d4c188204414262f9de6
dig <70334760+diganta121@users.noreply.github.com>
2024-06-19 22:36:13 +0530

    something

--- contloll_v1.5/contloll_v1.5.ino
+++ contloll_v1.5/contloll_v1.5.ino
@@ -8,24 +8,26 @@
 //a b c d
 //variables to store channel value:
 
-double ch3 = 2;//throttle ch3
-double ch1 = 3;//steer side-side ch1
-double ch2 = 4;//steer forward-back ch2
+double ch3 = 0;  //throttle ch3
+double ch1 = 0;  //steer side-side ch1
+double ch2 = 0;  //steer forward-back ch2
 
 
-int a = 10; int b = 9;`
-int c = 6; int d = 5;
+int a = 10;
+int b = 9;
+int c = 6;
+int d = 5;
 
 
+int forward_limt = 15;
+int side_limt = 15;
 
-int forward_lim = 25
-int side_lim = 25
+// vars
+int thr_inp = 0;
+int for_inp = 0;
+int side_inp = 0;
 
-
-// vars 
-int thr_inp = 0; int for_inp = 0; int side_inp = 0;
-
-int motorSpeed1 = 0; //variable to store motor speed:
+int motorSpeed1 = 0;  //variable to store motor speed:
 int motorSpeed2 = 0;
 
 
@@ -51,112 +53,156 @@ void setup() {
 }
 
 
-void loop()
-{
+void loop() {
 
-  for (int x=0; x<2; x++) {
-    ch1 += pulseIn(2, HIGH,3000); //speed
-    ch2 += pulseIn(3, HIGH,3000); //front-back
-    ch3 += pulseIn(4, HIGH,3000); //side-side
-  }
-  ch1 = ch1/2
-  ch2 = ch2/2
-  ch3 = ch3/2
-  
-  thr_inp = map(ch3, 1000, 2000, 0, 250); 
-  For_inp = map(ch1, 1000, 2000, -20, 20);
+  ch1 = pulseIn(2, HIGH, 3000);  //speed
+  ch2 = pulseIn(3, HIGH, 3000);  //front-back
+  ch3 = pulseIn(4, HIGH, 3000);  //side-side
+
+  Serial.print("ch: ");
+  Serial.print(ch1);
+  Serial.print(' ');
+  Serial.print(ch2);
+  Serial.print(' ');
+  Serial.print(ch3);
+  Serial.print(' ');
+  thr_inp = map(ch3, 1000, 2000, 0, 200);
+  for_inp = map(ch1, 1000, 2000, -20, 20);
   side_inp = map(ch2, 1000, 2000, -20, 20);
-
-  bool enabled = (thr_inp > 1 );
+  Serial.print("m: ");
+  Serial.print(thr_inp);
+  Serial.print(' ');
+  Serial.print(for_inp);
+  Serial.print(' ');
+  Serial.print(side_inp);
+  Serial.print('\n');
+  bool enabled = (thr_inp > 1);
 
   motorSpeed1 = 0;
   motorSpeed2 = 0;
 
-  if (enabled){
-    if (For_inp >= forward_lim){
+  if (enabled) {
+    if (for_inp >= forward_limt) {
       //forward
-      motorSpeed1 += 2 * for_inp * thr_inp;
-      motorSpeeds += 2 * for_inp * thr_inp;
+      motorSpeed1 += 2 + for_inp + thr_inp;
+      motorSpeed2 += 2 + for_inp + thr_inp;
 
-    }
-    else if (For_inp <= -forward_lim) {
-      motorSpeed1 += 2 * thr_inp;
-      motorSpeeds += 2 * thr_inp;
-    }
-    else{
-      stop()
+    } else if (for_inp <= -forward_limt) {
+      motorSpeed1 += 2 + thr_inp;
+      motorSpeed2 += 2 + thr_inp;
+    } else {
+      stop();
     }
 
     // sideways movement
-    if (side_inp > side_lim) {
+    if (side_inp > side_limt) {
       //RIGHT
-      motorSpeed1 -= (1 * thr_inp) - for_inp/2;
-      motorSpeeds += (2 * thr_inp) + for_inp/2;
-    }
-    else if (side_inp < -side_lim) {
+      motorSpeed1 -= for_inp / 2 + side_inp - for_inp;
+      motorSpeed2 += for_inp / 2 + side_inp + for_inp;
+    } else if (side_inp < -side_limt) {
       //LEFT
-      motorSpeed1 += (2 * thr_inp) + for_inp/2;
-      motorSpeeds -= (1 * thr_inp) - for_inp/2;
+      motorSpeed1 += for_inp / 2 + side_inp + for_inp;
+      motorSpeed2 -= for_inp / 2 + side_inp - for_inp;
     }
 
-  move()
-  }
-  else{
-    Stop();
+    Serial.println(String(motorSpeed1) + ' ' + String(motorSpeed2));
+    move();
+  } else {
+    stop();
     delay(200);
   }
- 
 }
 
-void move(){
+int mspeeds[9] = { 0, 25, 50, 100, 150, 200 };
+int mdelays[9] = { 100, 150, 300, 400, 500, 600 };
+
+void move() {
+  int idx = 0;
+  for (int x1 = 0; mspeeds[x1] >= abs(motorSpeed1); x1++) {
+    idx = x1;
+  }
+  if (motorSpeed1 > 0) {
+    digitalWrite(b, LOW);
+    motorWriteh(a, mdelays[idx]);
+
+  } else {
+    digitalWrite(b, LOW);
+    motorWriteh(b, mdelays[idx]);
+  }
+  idx = 0;
+  for (int x = 0; mspeeds[x] >= abs(motorSpeed2); x++) {
+    idx = x;
+  }
+  if (motorSpeed2 > 0) {
+    digitalWrite(d, LOW);
+    motorWriteh(c, mdelays[idx]);
+  } else {
+    digitalWrite(c, LOW);
+    motorWriteh(d, mdelays[idx]);
+  }
+
+
   // code to move the motor
   // and translate motorspeed
-  
+
   // int m1 = map(abs(motorSpeed1),0,5000, 0, 255);
   // int m2 = map(abs(motorSpeed1),0,5000, 0, 255);
 
-  if (motorspeed1 >= 0){
-    int m1 = map(motorSpeed1,0,5000, 0, 400);
-    motorWrite(a, int d)
-  }
-  else {
-  
-  }
-//====================
+  // if (motorSpeed1 >= 0){
+  //   int m1 = map(motorSpeed1,0,5000, 0, 400);
+  //   motorWrite(a, )
+
+  // }
+  // else {
+  //   int m1 = map(abs(motorSpeed1),0,4000, 0, 400);
+  //   motorWrite(a, )
+  // }
+
+
+
+  //====================
 }
 
-void light(int l1){
-  for (int l=0; l<l1; l++) { 
+void light(int l1) {
+  for (int l = 0; l < l1; l++) {
     delay(10);
-    digitalWrite(LED_BUILTIN,HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
-    digitalWrite(LED_BUILTIN,LOW);
+    digitalWrite(LED_BUILTIN, LOW);
     delay(10);
   }
 }
 
-void Stop() { //Stop:
+void stop() {  //Stop:
   motorSpeed1 = 0;
   motorSpeed2 = 0;
 
   light(1);
 }
 
-void motorWrite(int p, int d){
-  digitalWrite(p,LOW);
+void motorWriteh(int p, int d) {
+  digitalWrite(p, LOW);
   delay(10);
-  digitalWrite(p,HIGH);
+  digitalWrite(p, HIGH);
   delay(d);
-  digitalWrite(p,LOW);
+  digitalWrite(p, LOW);
   delay(10);
 }
 
+void motorWritel(int p, int d) {
+  digitalWrite(p, LOW);
+  delay(10);
+  digitalWrite(p, LOW);
+  delay(d);
+  digitalWrite(p, LOW);
+  delay(10);
+}
 
 // void Forward() { //Move Forward:
 
 //   digitalWrite(a, LOW); digitalWrite(b, HIGH);
 //   digitalWrite(c, LOW); digitalWrite(d, HIGH);
-  
+
 //   digitalWrite(13, HIGH); delay(400); digitalWrite(13, LOW);
 //   delay(100);
 // }
@@ -177,7 +223,7 @@ void motorWrite(int p, int d){
 
 //   digitalWrite(13, HIGH); delay(500); digitalWrite(13, LOW);
 //   delay(100);
-  
+
 //   /*
 //     motor1.run(FORWARD);                      //Drive motor1 clockwise:
 //     motor1.setSpeed(motorSpeed1);              //Define motor1 speed:
--- read_rc_val/read_rc_val.ino
+++ read_rc_val/read_rc_val.ino
@@ -1,18 +1,33 @@
-int channel1Pin = 0; // The analog pin used to read channel 1
-int channel2Pin = 1; // The analog pin used to read channel 2
+
 
 void setup() {
   Serial.begin(9600); // Start serial communication at 9600 bps
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  
 }
 
 void loop() {
-  int channel1Value = analogRead(channel1Pin); // Read the value of channel 1
-  int channel2Value = analogRead(channel2Pin); // Read the value of channel 2
+  
+  double ch1 = pulseIn(2, HIGH, 4000);  //speed
+  double ch2 = pulseIn(3, HIGH, 4000);  //front-back
+  double ch3 = pulseIn(4, HIGH, 4000);  //side-side
+  
+  int channel1Value = analogRead(3); 
+  int channel2Value = analogRead(4); 
 
-  // Print the values to the serial monitor
-  Serial.print("Channel 1 value: ");
-  Serial.println(channel1Value);
-  Serial.print("Channel 2 value: ");
+  Serial.print("ch: ");
+  Serial.print(ch1);
+  Serial.print(' ');
+  Serial.print(ch2);
+  Serial.print(' ');
+  Serial.print(ch3);
+  Serial.print(' ');
+
+  Serial.print("Analog ");
+  Serial.print(channel1Value);
+  Serial.print(' ');
   Serial.println(channel2Value);
 
   delay(100); // Wait for 100ms before reading again
